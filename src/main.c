c41f8c036807900fa0f375861233cfefcb6c0c0f
ivanTorda <ivan.torda@gmail.com>
2016-10-09 12:43:49 +0200

    cv2 uloha2, inicializacia tlacidla PC13

--- src/main.c
+++ src/main.c
@@ -1,36 +1,36 @@
 /**
-  ******************************************************************************
-  * @file    main.c 
-  * @author  MCD Application Team
-  * @version V1.0.0
-  * @date    11-February-2014
-  * @brief   Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    main.c
+ * @author  MCD Application Team
+ * @version V1.0.0
+ * @date    11-February-2014
+ * @brief   Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+ *
+ * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /** @addtogroup IO_Toggle
-  * @{
-  */ 
+ * @{
+ */
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -43,29 +43,25 @@ RCC_ClocksTypeDef RCC_Clocks;
 /* Private functions ---------------------------------------------------------*/
 
 /**
-  * @brief   Main program
-  * @param  None
-  * @retval None
-  */
+ * @brief   Main program
+ * @param  None
+ * @retval None
+ */
 int main(void) {
 	//init GPIOA
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+
 	GPIOA->MODER |= (0b01) << (5 * 2);
 	GPIOA->OTYPER &= ~(uint16_t) (1 << 5);
 	GPIOA->PUPDR |= (0b01) << (5 * 2);
 	GPIOA->OSPEEDR |= (0b11) << (5 * 2);
 
-	// PA5 ON
-	GPIOA->ODR |= (1 << 5);
-	// PA5 OFF
-	GPIOA->ODR &= ~(1 << 5);
-	// PA5 ON
-	GPIOA->BSRRL |= 1 << 5;
-	// PA5 OFF
-	GPIOA->BSRRH |= 1 << 5;
+	//init GPIOC
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	GPIOA->MODER |= (0b00) << (13 * 2);
+	GPIOA->OTYPER &= ~(uint16_t) (13 << 5);
+	GPIOA->PUPDR |= (0b00) << (13 * 2);
 
-	// PA5 TOOGLE
-	GPIOA->ODR ^= 1 << 5;
 
 	/* Infinite loop */
 	while (1) {
@@ -73,54 +69,51 @@ int main(void) {
 }
 
 /**
-* @brief  Inserts a delay time.
-* @param  nTime: specifies the delay time length, in 1 ms.
-* @retval None
-*/
-void Delay(__IO uint32_t nTime)
-{
-  TimingDelay = nTime;
-  
-  while(TimingDelay != 0);
+ * @brief  Inserts a delay time.
+ * @param  nTime: specifies the delay time length, in 1 ms.
+ * @retval None
+ */
+void Delay(__IO uint32_t nTime) {
+	TimingDelay = nTime;
+
+	while (TimingDelay != 0)
+		;
 }
 
 /**
-* @brief  Decrements the TimingDelay variable.
-* @param  None
-* @retval None
-*/
-void TimingDelay_Decrement(void)
-{
-  if (TimingDelay != 0x00)
-  { 
-    TimingDelay--;
-  }
+ * @brief  Decrements the TimingDelay variable.
+ * @param  None
+ * @retval None
+ */
+void TimingDelay_Decrement(void) {
+	if (TimingDelay != 0x00) {
+		TimingDelay--;
+	}
 }
 
 #ifdef  USE_FULL_ASSERT
 
 /**
-* @brief  Reports the name of the source file and the source line number
-*         where the assert_param error has occurred.
-* @param  file: pointer to the source file name
-* @param  line: assert_param error line source number
-* @retval None
-*/
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
-  /* User can add his own implementation to report the file name and line number,
-  ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  
-  /* Infinite loop */
-  while (1)
-  {
-  }
+{
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	/* Infinite loop */
+	while (1)
+	{
+	}
 }
 #endif
 
 /**
-* @}
-*/
-
+ * @}
+ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
